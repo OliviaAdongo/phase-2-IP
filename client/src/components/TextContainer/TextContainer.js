f9aac8c030f9d3c2121baae7fae989ce1f2e61ef
Olivia Adongo <akinyioliviatove@gmail.com>
2022-07-27 22:46:39 +0300

    Show active messages

--- client/src/components/TextContainer/TextContainer.js
+++ client/src/components/TextContainer/TextContainer.js
@@ -1,6 +1,32 @@
-import React, {useState, useEffect} from "react"; 
+import React from "react";
 
-const TextContainer = ()=>{
+
+
+const TextContainer = ({users})=>{
+    <div className="textContainer">
+    <div>
+        <h1>Chatterly App</h1>
+    </div>
+    {
+        users
+        ? (
+          <div>
+            <h1>People currently chatting:</h1>
+            <div className="activeContainer">
+              <h2>
+                {users.map(({name}) => (
+                  <div key={name} className="activeItem">
+                    {name}
+                    </div>
+                ))}
+                </h2>
+                </div>
+                </div>
+        ) :
+        null
+    }
+
+    </div>
 
 }
 export default TextContainer;
