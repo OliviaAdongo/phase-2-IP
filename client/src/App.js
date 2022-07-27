7f484ecaacb4fd9404c75d526fd054837ce8d70e
Olivia Adongo <akinyioliviatove@gmail.com>
2022-07-27 15:00:24 +0300

    Connection established

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import React from 'react'; 
 import './App.css';
+import Chat from './components/Chat/Chat';
+import Join from './components/Join/Join';
 
-//Routing component
+//Routing component Component  where the routing  will take place
 function App() {
   return (
     <div className="App">
