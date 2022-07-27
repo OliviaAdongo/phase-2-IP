bcee37bb812f9a3d50637367ac3bb683a23045a8
Olivia Adongo <akinyioliviatove@gmail.com>
2022-07-27 15:24:47 +0300

    Component for chatroom created

--- client/src/components/Join/Join.js
+++ client/src/components/Join/Join.js
@@ -1,6 +1,23 @@
 import React, {useState, useEffect} from "react"; 
 
 function Join(){
+const [name, setName] = useState('');
+const [room, setRoom] = useState('');
+
+return(
+    <div className="outerContainer">
+    <div className="innerContainer">
+        <h2>Join Room</h2>
+        <div>
+        <input placeholder="Name" className="joinInput" type="text" onChange={(event) => setName(event.target.value)} />
+        <input placeholder="Room" className="joinInput mt-20" type="text" onChange={(event) => setRoom(event.target.value)} />
+
+        <button className="signIn" type="submit">Sign In</button>
+        </div>
+    </div>
+
+    </div>
+)
 
 }
 
